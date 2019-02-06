3a71f426e87a2e573b01cff2c3f7b430ecb774f4
Brian Michael Witt <bmwitt1234@Brians-MacBook-Pro-2.local>
2019-02-06 14:53:07 -0500

    added html

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // Brian's Fizz Buzz with a BOOM!... with long winded if statements.
 // Try it here: https://jsbin.com/?js,console
-
+let compareNum1 = 3;
+let compareNum2 = 5;
 
 function renderListToHTML() {
     var f = "Fizz",
@@ -12,18 +13,18 @@ function renderListToHTML() {
 
     for (i = 1; i <= 101; i++) {
 
-        if (i % 3 == 0 && i % 5 == 0) {
+        if (i % compareNum1 == 0 && i % compareNum2 == 0) {
             console.log(f + b);
             $('.listDiv').append(f + b
 
             )
 
 
-        } else if (i % 3 == 0) {	// if there is no remainder, then i is divisible by 3
+        } else if (i % compareNum1 == 0) {	// if there is no remainder, then i is divisible by 3
             console.log(f);
             const fText = $(`<h1 class="fizzBuzz">${f}</h1>`)
             $(".listDiv").append(fText)
-        } else if (i % 5 == 0) {
+        } else if (i % compareNum2 == 0) {
             console.log(b);
             const bText = $(`<h1 class="fizzBuzz">${b}</h1>`)
             $(".listDiv").append(bText)
@@ -43,6 +44,30 @@ function renderListToHTML() {
     }
 
 }
+
+
+function changeFunction(event) {
+    event.preventDefault();
+    console.log("change button clicked");
+    let userinput1 = $('#input1').val();
+    let userinput2 = $('#input2').val();
+
+    console.log(userinput1, userinput2)
+
+    compareNum1 = userinput1;
+    compareNum2 = userinput2;
+
+    console.log("compare numbers after clicking change button " + compareNum1, compareNum2)
+
+
+    $(".listDiv").empty();
+    renderListToHTML();
+
+};
+
+$('#changeButton').on('click', changeFunction);
+
+
 
 
 renderListToHTML();
